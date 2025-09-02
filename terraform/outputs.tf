a5db632e58273b1ccf0b9dbd74756dc7eff6aabc
avdhendra <avdhendrasingh3@gmail.com>
2025-09-02 13:14:33 +0530

    remove eks cluster

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,10 +1,3 @@
-output "eks_cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
-
-output "eks_cluster_name" {
-  value = module.eks.cluster_name
-}
 
 output "ec2_instance_id" {
   value = module.ec2_instance.instance_id
@@ -14,10 +7,3 @@ output "ec2_instance_public_ip" {
   value = module.ec2_instance.public_ip
 }
 
-output "backend_bucket" {
-  value = var.backend_bucket
-}
-
-output "backend_dynamodb_table" {
-  value = var.backend_dynamodb_table
-}
