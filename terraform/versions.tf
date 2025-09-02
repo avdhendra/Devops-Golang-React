3883ca38f0693b8498de6baf2e03275849e2c822
avdhendra <avdhendrasingh3@gmail.com>
2025-09-02 14:05:46 +0530

    update regions

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -8,8 +8,9 @@ terraform {
   backend "s3" {
     bucket         = "my-tf-test-bucket"
     key            = "terraform.tfstate"
-    region         = "us-east-1"
-    use_lockfile= true
+    region         = "eu-central-1"               # Match the actual S3 bucket region
+    dynamodb_table = "tws-junnon-state-table"     # Still valid for state locking
+    encrypt        = true
 
   }
 
